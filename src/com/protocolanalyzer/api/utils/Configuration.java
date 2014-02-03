b2aa963708d76e82c81ea387267da62cc07ac745
Andres Torti <torti.max@gmail.com>
2014-02-03 00:05:26 -0300

    Correjido NullPointerException

--- src/com/protocolanalyzer/api/utils/Configuration.java
+++ src/com/protocolanalyzer/api/utils/Configuration.java
@@ -14,6 +14,7 @@ public class Configuration extends Properties{
 	
 	public boolean getBoolean (String key, boolean defaultValue) {
 		String p = getProperty(key);
+		if(p == null) return defaultValue; 
 		if(p.charAt(0) == 'b'){
 			if(p.charAt(1) == '1') return true;
 			else return false;
@@ -23,6 +24,7 @@ public class Configuration extends Properties{
 	
 	public int getInteger (String key, int defaultValue) {
 		String p = getProperty(key);
+		if(p == null) return defaultValue;
 		if(p.charAt(0) == 'i'){
 			return Integer.valueOf(p.substring(1));
 		}
